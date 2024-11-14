ac37229fcba0a3f88bfb627df688e043916b9172
Madhu-oss875 <madhusmita55.behera@gmail.com>
2024-11-14 21:03:43 +0530

    Added the files

---
+++ src/sept/exe_09092024/Lab007.java
@@ -0,0 +1,10 @@
+package sept.exe_09092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+
+
+
+        System.out.println();
+    }
+}
---
+++ src/sept/exe_11092024/Lab011.java
@@ -0,0 +1,14 @@
+package sept.exe_11092024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+
+
+        boolean b = true;
+        boolean _ = true;
+        boolean $ = true;
+        boolean madhu123 = false;
+        System.out.println(madhu123);
+
+    }
+}
