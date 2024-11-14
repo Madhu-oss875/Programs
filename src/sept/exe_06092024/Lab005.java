a0e1816e9cb570aeeb50ee26f27849cff2c30183
Madhu-oss875 <madhusmita55.behera@gmail.com>
2024-11-14 18:02:02 +0530

    Added the files

---
+++ src/sept/exe_06092024/Lab005.java
@@ -0,0 +1,14 @@
+package sept.exe_06092024;
+
+import java.sql.SQLOutput;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+    }
+}
---
+++ src/sept/exe_06092024/Lab006.java
@@ -0,0 +1,13 @@
+package sept.exe_06092024;
+
+public class Lab006 {
+    public static void main(String[] Madhu) { // argument can be anything there is no problem
+        System.out.println("Hello cuty Madhu");
+
+// This code will not be executed
+
+        /*
+        Multi line comment
+         */
+    }
+}
